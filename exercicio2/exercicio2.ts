313d92d9ce182886c8b8428fc92660f5db5b4ef6
JoaoPedroBackXavier <joaopedro.backxavier@gmail.com>
2023-06-21 22:01:40 -0300

    lalala

---
+++ exercicio2/exercicio2.ts
@@ -0,0 +1,6 @@
+function itcv(): void {
+    const cor1 = prompt("insira sua primeira cor favorita")
+    const cor2 = prompt("insira sua segunda cor favorita")
+    const cor3 = prompt("insira sua terceira cor favorita")
+    console.log(cor1, cor2, cor3)
+}
